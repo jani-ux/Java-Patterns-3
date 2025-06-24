2a23881accd63b2342fbf45b82ffa4398b757473
jani-ux <janithpasiduuniinfo@gmail.com>
2025-06-24 09:50:03 +0530

    Update numberpatterns.java

--- numberpatterns.java
+++ numberpatterns.java
@@ -245,5 +245,17 @@ public class NumberPatterns {
             System.out.println(" ");
         }
         System.out.println("");
+        
+        //Pyramid
+        System.out.println("Pyramid");
+        for(int i=1; i<=n; i++){
+            for(int j=n; j>i; j--){
+                System.out.print("  ");
+            }
+            for(int j=1; j<= 2*i -1 ; j++){
+            System.out.print(" " + j);
+        }
+            System.out.println(" ");
+        }
     }
 }
